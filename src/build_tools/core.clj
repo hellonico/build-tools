0a91bb575ca08994a0a6d5cc683c6edfb00000b7
Nicolas Modrzyk <hellonico@gmail.com>
2025-01-26 16:18:35 +0900

    support for parallel models and prompts

--- src/build_tools/core.clj
+++ src/build_tools/core.clj
@@ -1,10 +1,11 @@
 (ns build-tools.core
   (:require
     [clojure.java.io :as io]
-    [clojure.pprint]
     [clojure.java.shell :as shell]
+    [clojure.pprint]
     [clojure.string :as str]
-    [clojure.tools.build.api :as b]))
+    [clojure.tools.build.api :as b]
+    [deps-deploy.deps-deploy :as deploy]))
 
 ;(def app-name "linen")
 (def version (format "1.0.%s" (or (b/git-count-revs nil) "no.git")))
@@ -17,8 +18,11 @@
 (defn clean [_]
   (b/delete {:path "target"}))
 
-(defn uber-file[app-name]
+(defn uber-file [app-name]
   (format "target/%s-%s-jdk%s.jar" (str/lower-case app-name) version (System/getProperty "java.version")))
+
+(defn lib-name [group-name app-name]
+  (symbol (str/lower-case (str group-name "/" app-name))))
 
 (defn uberjar [{:keys [app-name compile mainns] :as options}]
   ;(clojure.pprint/pprint options)
@@ -29,6 +33,10 @@
   (b/compile-clj {:basis      @basis
                   :ns-compile compile
                   :class-dir  class-dir})
+  (b/write-pom {:class-dir class-dir
+                :lib       (lib-name "pyjama" app-name)
+                :version   version
+                :basis     basis})
   (b/uber {:class-dir class-dir
            :uber-file (uber-file app-name)
            :basis     @basis
@@ -36,13 +44,13 @@
            }))
 (def uberize uberjar)
 
-(defn app-icon[{app-name :app-name}]
-  (let[ os (System/getProperty "os.name")
+(defn app-icon [{app-name :app-name}]
+  (let [os (System/getProperty "os.name")
         file-extension (if (or (.contains os "Mac") (= os "Mac OS X")) ".icns" ".png")
-       ]
-    (str "resources/" (str/lower-case app-name) file-extension)
-  ))
-(defn jpackage [{app-name :app-name :as options} ]
+        ]
+    (str "resources/" (str/lower-case app-name) file-extension)))
+
+(defn jpackage [{app-name :app-name :as options}]
   (shell/sh "jpackage"
             "--dest" "output"
             "--name" app-name
@@ -52,9 +60,21 @@
             "--icon" (app-icon options)
             "--app-version" version))
 
-(defn build-all[options]
+(defn build-all [options]
   (uberize options)
   (jpackage options))
+
+(defn upload [{app-name :app-name :as options}]
+  (deploy/deploy {:artifact       (uber-file app-name)
+                  :version        version
+                  :installer      :remote
+                  :sign-releases? false
+                  :repository     {:mvn/repos {;:id       "my"
+                                               :url      (System/getenv "NEXUS_URL")
+                                               :username (System/getenv "NEXUS_USERNAME")
+                                               :password (System/getenv "NEXUS_PASSWORD")
+                                               }}
+                  :pom-file       (str class-dir "/META-INF/maven/" (lib-name "pyjama" app-name) "/pom.xml")}))
 
 ;(defn uber-linen [_]
 ;  (uberize {:app-name "linen" :compile '[linen.core] :main 'linen.core}))
